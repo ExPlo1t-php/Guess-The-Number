f0f40446f1e81f0cb913f2f5c859f9faca319fb7
ExPlo1t-php <57189045+ExPlo1t-php@users.noreply.github.com>
2021-11-26 16:54:21 +0100

    Add files via upload

--- Guess The Number/script.js
+++ Guess The Number/script.js
@@ -1,20 +1,26 @@
+//this function declares a random number between 1 and 100, and then assign it to the variable initiator.
+     function initiatorfx() {
+          return Math.floor(Math.random() *100 );
+        };
+        let initiator = initiatorfx();
 
 
 function checker(){
-     
-     let initiator = function () {
-          return Math.floor(Math.random() * (100 - 1) + 1);
-        };
-
-
+ 
+          
     let input = document.getElementById("input").value;
+        for (let i = 1 ; i <= 10 ; i++) {
+                 let counter = 10;
+     document.getElementById("tries").innerHTML= counter - 1;
+          if(initiator == input && i <= 3){
+               document.getElementById("message").innerHTML="Bravo, vous etes un Génie al7mar !!!";
+          }else if(input <= initiator){
+               document.getElementById("message").innerHTML="plus petite";
+          }else if(input >= initiator){
+               document.getElementById("message").innerHTML="plus grande";
+          }else{
+               document.getElementById("message").innerHTML="Please kill yourself"; }
+        }
 
-if(initiator === input){
-     document.getElementById("message").innerHTML="sucess";
-} else if(initiator <= input){
-     document.getElementById("message").innerHTML="bad";
-}else{
-     document.getElementById("message").innerHTML="bad";
-}
 }
 
